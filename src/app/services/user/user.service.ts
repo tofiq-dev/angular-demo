5c5b6f68dadb830ae6660d9948d535a2478e06bf
gerasaka <gerryjulioo@gmail.com>
2024-11-11 05:41:51 +0700

    feat: create user service

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUser } from './user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private http = inject(HttpClient);
+
+  private BASE_API_PATH = 'https://jsonplaceholder.typicode.com';
+
+  getUsers() {
+    return this.http.get<IUser[]>(this.BASE_API_PATH + '/users');
+  }
+}
